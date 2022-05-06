453d03c4ecf7267a97c5390c66e5014b8f263518
Pedro A.F Paffaro <pedroafpaffaro@gmail.com>
2022-05-06 20:22:36 -0300

    Changing files

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,26 +36,71 @@ function darkMode(){
     var switchBtn = document.getElementById("switch");
     var page = document.getElementById("page");
     var menu = document.querySelector("#menu");
+    var btnSignIn = document.getElementById("btn-login");
+    var btnSignUp = document.getElementById("btn-signup");
 
     if(switchBtn.value == 1){
+        localStorage.setItem('theme', 'dark');
+
         switchBtn.style.transitionDuration = ".5s";
         switchBtn.style.background = "#4bd865";
 
         page.style.transitionDuration = ".2s";
         page.style.backgroundColor = "rgba(37, 35, 35, 0.897)";
         page.style.color = "#f8f8ff";
+
         menu.style.transitionDuration = ".2s";
         menu.style.backgroundColor = "rgba(37, 35, 35, 0.897)";
+
+        btnSignIn.style.backgroundColor = "#222";
+        btnSignIn.style.color = "#f8f8ff";
+    
+        btnSignUp.style.backgroundColor = "#222";
+        btnSignUp.style.color = "#f8f8ff";
     }
     else{
+        localStorage.setItem('theme', 'light');
+
         switchBtn.style.transitionDuration = ".5s";
         switchBtn.style.background = "#e9e9ea";
 
         page.style.transitionDuration = ".2s";
         page.style.backgroundColor = "#f8f8ff";
         page.style.color = "rgba(37, 35, 35, 0.897)";
+
         menu.style.transitionDuration = ".2s";
         menu.style.backgroundColor = "#f8f8ff";
+        
+        btnSignIn.style.transitionDuration = ".2s";
+        btnSignIn.style.backgroundColor = "#f8f8ff";
+        btnSignIn.style.color = "#000";
+    
+        btnSignUp.style.transitionDuration = ".2s";
+        btnSignUp.style.backgroundColor = "#f8f8ff";
+        btnSignUp.style.color = "#000";
+    }
+}
+
+function darkTheme(){
+    var page = document.getElementById("page");
+    var forms = document.querySelector(".form-cadastro");
+    var textSignUpWith = document.querySelectorAll(".sign-up-with");
+
+    if(localStorage.getItem('theme') == 'dark'){
+        page.style.transitionDuration = ".2s";
+        page.style.backgroundColor = "rgba(37, 35, 35, 0.897)";
+        page.style.color = "#f8f8ff";
+        
+        forms.style.backgroundColor = "#222";
+        forms.style.color = "#f8f8ff";
+
+        textSignUpWith[0].style.color = "#f8f8ff";
+        textSignUpWith[1].style.color = "#f8f8ff";
+    }
+    else{
+        page.style.transitionDuration = ".2s";
+        page.style.backgroundColor = "#f8f8ff";
+        page.style.color = "rgba(37, 35, 35, 0.897)";
     }
 }
 
